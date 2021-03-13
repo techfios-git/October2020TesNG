289f64710d87ff1cf725dfcbd6be83ac51b7a1b2
shihabamin <amin.khandaker@gmail.com>
2021-03-13 12:44:44 -0600

    third commit

---
+++ src/main/java/variousConcepts/TestGit1.java
@@ -0,0 +1,18 @@
+package variousConcepts;
+
+public class TestGit1 {
+	
+	public static void main(String[] args) {
+		printMe();
+		printMe1();
+	}
+	
+	public static void printMe() {
+		System.out.println("test git");
+	}
+	
+	public static void printMe1() {
+		System.out.println("test git 1");
+	}
+
+}
